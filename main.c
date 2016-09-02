2dd3e35f7bfc51b8ac2335e9cf4086a5c59d14db
cblakevill <cblakevill@gmail.com>
2016-09-03 02:54:18 +0800

    Update main.c

--- main.c
+++ main.c
@@ -16,17 +16,49 @@
 #define AXIS_Y BIT1
 #define AXIS_Z BIT2
 
-int bool_button = 0; //false
+#define DEBOUNCE_TIME_CONST 1600 //200ms debounce
+
+void initialise(void);
+void disableButtons(void);
+void enableButtons(void);
+void debounce(void);
+void updateTemp(int);
+void updateAcc(void);
+void updateLEDs(void);
+
+int bool_button = 1;
+unsigned int x_bit, y_bit, z_bit;
+unsigned int TempInitial = 0;
+unsigned int TempMeasure = 0;
+unsigned int TempResult = 0;
 
 int main(void)
 {
-
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
-	
+    initialise();
+    updateTemp(1);
+
+	while(1)
+	{
+		updateAcc();
+		updateTemp(0);
+		if(bool_button)
+			updateLEDs();
+		else
+		{
+			//clear display
+			PJOUT = 0x00;
+			P3OUT = 0x00;
+		}
+	}
+}
+
+void initialise(void)
+{
     //Initialise LEDS
 	PJDIR |= (LED1 + LED2 + LED3 + LED4);
-	P3DIR |= (LED5+LED6+LED7+LED8);
-	PJOUT &= P1IN;
+	P3DIR |= (LED5 + LED6 + LED7 + LED8);
+	PJOUT &= ~(LED1 + LED2 + LED3 + LED4);
 	P3OUT &= ~(LED5+LED6+LED7+LED8);
 
 	P4DIR &= 0x00; //PUSH BUTTONS as input
@@ -34,7 +66,6 @@ int main(void)
 	P4REN = (PUSH1 + PUSH2); //Enable resitor
 	P4OUT = (PUSH1 + PUSH2); //Pull-up resistor
 	P4IFG &= 0x00; //Initialise interrupt flag
-	__enable_interrupt();
 
 	//enable accelerometer and NTC
 	P2DIR |= BIT7;
@@ -42,51 +73,106 @@ int main(void)
 
 	//intialise accelerometer
 	P3DIR &= ~(AXIS_X + AXIS_Y + AXIS_Z); //P3.0, P3.1, P3.2 input
-	P3IN &= 0x00; //initialise input
 
 	//intialise thermistor
 	P1DIR &= ~(BIT4); //P1.4 input
-	P1IN &= 0x00; //initialise input
+	P1OUT &= ~(BIT4);
 
-	int x_bit, y_bit, z_bit;
-	int ntc;
+	//ADC input channel 4 (A4)
+	P1SEL0 = BIT4;
+	P1SEL1 = BIT4;
 
-	while(1)
-	{
-		//update NTC
-		ntc = P1IN;
-
-		//update accelerometer input
-		x_bit = (P3IN & AXIS_X);
-		y_bit = (P3IN & AXIS_Y);
-		z_bit = (P3IN & AXIS_Z);
-
-		//x_bit: P3IN.0  -> P3OUT.7   (<< 7)
-		//y_bit: P3IN.1  -> P3OUT.6   (<< 5)
-		//z_bit: P3IN.2  -> P3OUT.5   (<< 3)
-		//update accelerometer
-		P3OUT = ((x_bit << 7) + (y_bit << 5) + (z_bit << 3));
-
-		//update button controlled LEDS OR NTC
-		if(bool_button)
-			//display NTC
-			PJOUT = ntc;
-		else
-			//blank display
-			PJOUT &= 0x00;
-	}
+	//Initialise ADC
+	//ADC initialisation from https://github.com/jaspreetsingh009/python_msp430/blob/guicode.py/msp430_code.c
+	ADC10CTL0 &= ~ADC10ENC; //disable conversion
+	ADC10CTL0 = ADC10ON + ADC10SHT_5; //enable adc + sample hold select 5
+	ADC10CTL1 = ADC10SHS_0 + ADC10SHP + ADC10CONSEQ_0 + ADC10SSEL_0;
+	ADC10MCTL0 = ADC10SREF_0 + ADC10INCH_4; //reference 0 + input A4
+	
+	__enable_interrupt();
 }
 
+//ISR for butttons
 #pragma vector = PORT4_VECTOR
 __interrupt void P4_ISR(void)
 {
-	if(P4IFG & BIT0)
-	{
-		P4IFG &= ~(BIT0);
-		bool_button = ~bool_button; //toggle PJOUT output (NTC or blank)
-	}
+
+	disableButtons();
+
+	P4IFG &= ~(BIT0 + BIT1);
+	bool_button ^= 1; //toggle PJOUT output (either NTC or all high)
+
+	debounce(); //200ms debounce
+}
+
+//ISR for timer0 debouncing
+#pragma vector = TIMER1_A0_VECTOR
+__interrupt void timer0_ISR(void)
+{
+	TA1CCTL0 = 0x00; //TACCR0 interrupt disabled
+	TA1CTL = 0x00; // clock off
+	enableButtons(); //debounce finished
+}
+
+void disableButtons(void)
+{
+	P4IFG = 0x00; //clear flag
+	P4IE = 0x00; //disable interrupt
+}
+
+void enableButtons(void)
+{
+	P4IFG = 0x00; //clear flag
+	P4IE |= (PUSH1 + PUSH2); //enable interrupt
+}
+
+void debounce(void)
+{
+	TA1CCTL0 = CCIE; //TACCR0 interrupt enabled
+	TA1CCR0 = DEBOUNCE_TIME_CONST; //rise up time of 200ms
+	TA1CTL = TASSEL_1 + MC_1; //ACLK + up mode
+}
+
+
+void updateTemp(int firstTime)
+{
+	__disable_interrupt();
+	//enable and start ADC conversion
+	ADC10CTL0 |= ADC10ENC + ADC10SC;
+	while (ADC10CTL1 & BUSY);
+
+	if(firstTime)
+		TempInitial = ADC10MEM0; //reference point for future measurements
 	else
-	{
-		P4IFG &= ~(BIT0 + BIT1);
-	}
+		TempMeasure = ADC10MEM0;
+
+	//end ADC conversion
+	ADC10CTL0 &= ~ADC10ENC;
+	__enable_interrupt();
+}
+
+void updateLEDs(void)
+{
+	//update accelerometer LEDs
+	//x_bit: P3IN.0  -> P3OUT.7   (<< 7)
+	//y_bit: P3IN.1  -> P3OUT.6   (<< 5)
+	//z_bit: P3IN.2  -> P3OUT.5   (<< 3)
+	//update accelerometer LEDs
+	P3OUT = ((x_bit << 7) + (y_bit << 5) + (z_bit << 3));
+
+	//update thermistor LEDs
+	TempResult = TempMeasure - TempInitial;
+
+	if(TempResult < 160)
+		PJOUT = 0x00;
+	else
+		PJOUT = BIT0;
+}
+
+void updateAcc(void)
+{
+	//update accelerometer input
+	x_bit = (P3IN & AXIS_X);
+	y_bit = (P3IN & AXIS_Y);
+	z_bit = (P3IN & AXIS_Z);
 }
